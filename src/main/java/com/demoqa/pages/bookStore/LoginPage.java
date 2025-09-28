e26d4e8a8824bfb018b6f116340a54d0f768b955
Evgeny Polonsky <evgpolon@gmail.com>
2025-09-28 19:06:41 +0300

    first

---
+++ src/main/java/com/demoqa/pages/bookStore/LoginPage.java
@@ -0,0 +1,10 @@
+package com.demoqa.pages.bookStore;
+
+import com.demoqa.pages.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePage {
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ src/test/java/com/demoqa/tests/LoginTests.java
@@ -0,0 +1,24 @@
+package com.demoqa.tests;
+
+import com.demoqa.pages.HomePage;
+import com.demoqa.pages.SidePanel;
+import com.demoqa.pages.bookStore.LoginPage;
+import com.demoqa.pages.bookStore.ProfilePage;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase{
+
+    @BeforeMethod
+    public void precondition(){
+        new HomePage(driver).getBookStore();
+        new SidePanel(driver).selectLogin();
+    }
+
+    @Test
+    public void loginPositiveTest(){
+        new LoginPage(driver).enterUserData("Jojomojo","Aa1234567!").clickOnLoginButton();
+        new ProfilePage(driver).verifyUserName("Jojomojo");
+    }
+
+}
